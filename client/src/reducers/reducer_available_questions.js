da46d2c92ac1187f8f332311066f6d254fb2355d
Richard Artoul <richardartoul@gmail.com>
2016-04-03 13:18:23 -0700

    Make client directory

--- client/src/reducers/reducer_available_questions.js
+++ client/src/reducers/reducer_available_questions.js
@@ -4,6 +4,6 @@ export default function() {
   return List.of(
     "Tell me a little about yourself.",
     "Whats the most interesting thing you've learned recently?",
-    "Whats your favorite new experimental technology?",
+    "Whats your favorite new experimental technology?"
   );
 }
